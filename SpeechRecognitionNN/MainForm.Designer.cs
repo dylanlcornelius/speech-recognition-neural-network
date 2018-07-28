eed070a7b8088f3b3a9da3840dcb7f8b0040756d
Dylan <dylanlcornelius@gmail.com>
2018-07-28 10:20:14 -0400

    Form Creation

--- SpeechRecognitionNN/MainForm.Designer.cs
+++ SpeechRecognitionNN/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SpeechRecognitionNN
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.DarkGray;
+            this.btnRun.Location = new System.Drawing.Point(713, 12);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnRun);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech Recognition Neural Network";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRun;
     }
 }
 
---
+++ SpeechRecognitionNN/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeechRecognitionNN
+{
+    public partial class MainForm : Form
+    {
+        bool isRunning;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            isRunning = false;
+
+            
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (isRunning = !isRunning)
+            {
+
+                btnRun.BackColor = Color.Red;
+            }
+            else
+            {
+
+                btnRun.BackColor = Color.DarkGray;
+            }
+        }
+    }
+}
